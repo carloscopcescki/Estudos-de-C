019572c34da79c54158e95778d95cbf5875b1d27
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2024-09-04 16:13:45 -0300

    Update lista_encadeada.c

--- Lista Encadeada/lista_encadeada.c
+++ Lista Encadeada/lista_encadeada.c
@@ -35,6 +35,28 @@ No * imprimeFinal(No * lista, int valor){
     }
 }
 
+No * remover(No * lista, int valor){
+    No * ant = NULL;
+    No * p = lista;
+    while (p != NULL && p->info != valor){
+        ant = p;
+        p = p->proximo;
+    }
+    if (p==NULL){
+        return lista;
+    }
+
+    if (p==lista){
+        lista = p->proximo; // Remove no inicio
+    }
+    else{
+        ant->proximo = p->proximo;  // Remove no final
+    }
+
+    free(p);
+    return lista;
+}
+
 void imprimirLista(No * lista){
     No * p;
     p = lista;
@@ -47,12 +69,20 @@ void imprimirLista(No * lista){
 int main(){
     No* listaEncadeada;
     listaEncadeada = listaVazia();
-    listaEncadeada = imprimeInicio(listaEncadeada, 20);
-    listaEncadeada = imprimeInicio(listaEncadeada, 15);
-    listaEncadeada = imprimeFinal(listaEncadeada, 35);
-    listaEncadeada = imprimeFinal(listaEncadeada, 98);
+    listaEncadeada = imprimeInicio(listaEncadeada, 2);
+    listaEncadeada = imprimeInicio(listaEncadeada, 1);
+    listaEncadeada = imprimeFinal(listaEncadeada, 3);
+    listaEncadeada = imprimeFinal(listaEncadeada, 4);
     
     printf("\nLista Encadeada: ");
+    imprimirLista(listaEncadeada);
+
+    printf("\nRemove no inicio: ");
+    listaEncadeada = remover(listaEncadeada, 1);
+    imprimirLista(listaEncadeada);
+
+    printf("\nRemove no final: ");
+    listaEncadeada = remover(listaEncadeada, 4);
     imprimirLista(listaEncadeada);
 
     return 0;
